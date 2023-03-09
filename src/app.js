4a55d2559cdae629c07144fc4e747e9c997be4a7
gustavosidoti <gustavosidoti@gmail.com>
2023-03-09 01:51:55 +0100

    inicial

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
+const path=require('path');
 const express=require('express');
-const PORT=8080;
 const engine  = require('express-handlebars').engine;
 const Server = require('socket.io').Server;
-const path=require('path');
+
+const PORT=8080;
+
+const app=express();
 
 // borrar esto
 const fs = require ('fs');
@@ -10,51 +13,36 @@ const pathProducts = path.join(__dirname,'./Products.json');
 const productsOn = fs.readFileSync(pathProducts, 'utf8');
 const products = JSON.parse(productsOn);
 
-const app=express();
 
 // Motor de plantillas
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
-app.set('views', './src/views');
+app.set('views', path.join(__dirname,'./views'));
 
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 // Servir Contenido de la carpeta public 
-app.use(express.static('./src/public/assets/js'));
-
-const realTimeRouter = require('./routes/realtimeproducts.router');
-const productsRouter = require('./routes/products.router');
-
-// Rutas
-app.use('/api/products', (req, res, next) => {
-    req.serverSocket = io;
-    next();
-}, productsRouter);
-
-app.use('/api/carts', require('./routes/carts.router'));
-//app.use('/api/realtimeproducts', require('./routes/realtimeproducts.router'));
-
-app.use('/api/realtimeproducts', (req, res, next) => {
-    req.serverSocket = io;
-    next();
-}, realTimeRouter)
+app.use(express.static(path.join(__dirname,'./src/public')));
 
 
-app.use('/', require('./routes/views.router'));
+app.use('/', (req, res) =>{
+    res.setHeader('Content-Type','text/html');
+    res.status(200).render('home');
+});
 
 
-const serverhttp=app.listen(PORT,()=>{
+const serverHttp=app.listen(PORT,()=>{
     console.log(`Server escuchando en puerto PORT ${PORT}`);
 });
 
-const io = new Server(serverhttp);
-io.on('connection', (socket) => {
+const serverSockets = new Server(serverHttp);
+    serverSockets.on('connection', (socket)=>{
 
     console.log(socket.handshake);
 
-    console.log(`Se han conectado ${socket.id}`);
-});
+    console.log(`Se han conectado, socket id ${socket.id}`)
+})
 
-serverhttp.on('error',(error)=>console.log(error));
+serverHttp.on('error',(error)=>console.log(error));
